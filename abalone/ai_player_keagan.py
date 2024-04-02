0e0f9ddf3994e2294898fc5c56d76c92fed14fc5
Keagan <keaganpurtell@hotmail.com>
2024-04-01 22:54:36 -0700

    start of ai

--- abalone/ai_player_keagan.py
+++ abalone/ai_player_keagan.py
@@ -10,7 +10,37 @@ from enums import Space, Direction
 from game import Game
 
 
+
+
 class AiPlayerKeagan(AbstractPlayer):
+
+
+    def minimax(self, game, depth, alpha, beta, maximizingPlayer):
+        if depth == 0 or game.is_game_over():
+            return self.heuristic_evaluation(game)
+        
+        if maximizingPlayer:
+            maxEval = float('-inf')
+            for move in game.generate_legal_moves():
+                eval = self.minimax(game, depth - 1, alpha, beta, False)
+                maxEval = max(maxEval, eval)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+            return maxEval
+        else:
+            minEval = float('inf')
+            for move in game.generate_legal_moves():
+                eval = self.minimax(game, depth - 1, alpha, beta, True)
+                minEval = min(minEval, eval)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+            return minEval
+        
+    def heuristic_evaluation(self, game):
+        return 0
+
     def turn(self, game: Game, moves_history: List[Tuple[Union[Space, Tuple[Space, Space]], Direction]], selection_lock: bool) \
             -> Tuple[Union[Space, Tuple[Space, Space]], Direction]:
 
@@ -23,10 +53,18 @@ class AiPlayerKeagan(AbstractPlayer):
 
         # our ai code is here....
         #1. minimax with alpha-beta pruning
+        self.minimax(game, 3, float('-inf'), float('inf'), True)
+
+
         #2. heuristic evaluation function
+        self.heuristic_evaluation(game)
+
+
         #3. choice of:
         #   a. node ordering
         #   b. transposition tables
         #   c. quiescence search
+            
+        hueristic_choice = self.take_best_move(legal_moves)
 
-        return choice(legal_moves)
+        return hueristic_choice
